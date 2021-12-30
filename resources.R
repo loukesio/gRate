d3c2b041bd33c5afedc176c75201096e04e78380
Loukas Theodosiou <theodosiou@evolbio.mpg.de>
2021-12-30 12:41:06 +0100

    :whale: discover the resources

---
+++ resources.R
@@ -0,0 +1,9 @@
+# links to follow 
+https://stats.stackexchange.com/search?q=r+nonline%2A+logis%2A+growth+answers%3A1
+https://stats.stackexchange.com/questions/514788/logistic-growth-curve-with-r-nls
+https://stats.stackexchange.com/questions/47802/whats-the-most-pain-free-way-to-fit-logistic-growth-curves-in-r
+https://cran.r-project.org/web/packages/deBInfer/vignettes/logistic_ode_example.pdf
+https://cran.r-project.org/web/packages/deBInfer/vignettes/logistic_ode_example.pdf
+https://rpubs.com/Swarchal/phenoScreen
+https://rstudio-pubs-static.s3.amazonaws.com/427185_abcc25a951c9436680dc6a8fcc471ca9.html
+https://lokraj.me/post/create-96-well-plate/
